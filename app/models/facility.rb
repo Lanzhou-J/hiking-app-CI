9719f5e75f00fc41877aceeb7b4584e1e33e7f0e
Edward Deam <edwardcrdeam@gmail.com>
2020-07-01 20:47:50 -0700

    facility spec passing

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,6 @@
 class Facility < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :facility_trails
+  has_many :trails, through: :facility_trails
 end
--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Facility, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:facility) }
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(build(:facility)).to be_valid
+    end
+
+    it { expect(subject).to validate_presence_of(:name) }
+  end
+
+  context 'association' do
+    it { expect(subject).to have_many(:trails).through(:facility_trails) }
+  end
 end
