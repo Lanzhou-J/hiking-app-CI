f824c1a706be13fec9a82a7409b3c11e37e3b60c
Edward Deam <edwardcrdeam@gmail.com>
2020-07-01 20:48:10 -0700

    facility trails spec

--- spec/models/facility_trail_spec.rb
+++ spec/models/facility_trail_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe FacilityTrail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(build(:facility_trail)).to be_valid
+    end
+  end
+
+  context 'associations' do
+    it { expect(subject).to belong_to(:trail) }
+    it { expect(subject).to belong_to(:facility) }
+  end
 end
