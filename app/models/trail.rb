66dbfaafa64d5ff08855068795c6fc2363fc475a
Edward Deam <edwardcrdeam@gmail.com>
2020-07-01 20:37:57 -0700

    trails specs passing

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,6 +1,8 @@
 class Trail < ApplicationRecord
   validates :name, presence: true
-  validates :difficulty, numericality: {less_than: 5}
+  validates_length_of :name, maximum: 75, minimum: 8
+  validates :difficulty, numericality: {less_than: 5, greater_than: 0}
+  validates :distance, numericality: {greater_than: 0}
   has_many :facility_trails
   has_many :facilities, through: :facility_trails
 end
--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Trail, type: :model do
   subject { build(:trail) }
-
+  
   context 'validations' do
     it 'has a valid factory' do
       expect(build(:trail)).to be_valid
     end
-
+    
     it { expect(subject).to validate_presence_of(:name) }
-    it { expect(subject).to validate_numericality_of(:difficulty).is_less_than(5) }
+    it { expect(subject).to validate_length_of(:name).is_at_least(8).is_at_most(75) }
+    it { expect(subject).to validate_numericality_of(:difficulty).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(5) }
+    it { expect(subject).to validate_numericality_of(:distance).is_greater_than(0) }
+
     # Other syntax that is valid
     #it { should validate_presence_of(:name)}
     #it { is_expected.to validate_presence_of(:name)}
