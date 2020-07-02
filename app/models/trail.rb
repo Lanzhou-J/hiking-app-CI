eb6b2fdec51236635350dd00d91072e8bcfe0cd1
Edward Deam <edwardcrdeam@gmail.com>
2020-07-01 20:51:26 -0700

    difficulty trails validation fix

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,7 +1,7 @@
 class Trail < ApplicationRecord
   validates :name, presence: true
   validates_length_of :name, maximum: 75, minimum: 8
-  validates :difficulty, numericality: {less_than: 5, greater_than: 0}
+  validates :difficulty, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
   validates :distance, numericality: {greater_than: 0}
   has_many :facility_trails
   has_many :facilities, through: :facility_trails
