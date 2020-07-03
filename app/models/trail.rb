31b538156009ba94e630f5f65297ac7ed632eef2
Edward Deam <edwardcrdeam@gmail.com>
2020-07-02 20:53:02 -0700

    update factories

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -3,6 +3,8 @@ class Trail < ApplicationRecord
   validates_length_of :name, maximum: 75, minimum: 8
   validates :difficulty, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
   validates :distance, numericality: {greater_than: 0}
+
   has_many :facility_trails
   has_many :facilities, through: :facility_trails
+  belongs_to :user
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
+    sequence :email do |n|
+       "testuser#{n}@test.com" 
+    end
+    password { "password123" }
   end
 end
